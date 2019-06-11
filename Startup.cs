f2bc35aa80ef8ba5a12d5c122e8a2f3342bd4b36
botframework <util@botframework.com>
2019-06-11 15:15:33 +0000

    Changed errors?

--- Startup.cs
+++ Startup.cs
@@ -45,7 +45,7 @@ namespace Microsoft.BotBuilderSamples
             services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
             
             // Helper code that makes the actual HTTP calls to QnA Maker. It is injectable for local unit testing.
-            services.AddHttpClient<IQnAService, QnAService>();
+            //services.AddHttpClient<IQnAService, QnAService>();
             
             // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.) 
             services.AddSingleton<IStorage, MemoryStorage>();

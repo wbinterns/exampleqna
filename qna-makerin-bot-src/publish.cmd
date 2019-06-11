56b4dae71f7d211b4227d5eb07771174d37f814a
wbinterns <jquinzon@allete.com>
2019-06-11 11:00:38 -0500

    Fixed duplicates

--- qna-makerin-bot-src/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=qna-makerin-bot-Web-Deploy.pubxml -p:Password=m09Z1qkpWquoirMxocXewhedr3NhyyeHG1raKhcwBQziBYXSMbNMYwlyBDmz
-
